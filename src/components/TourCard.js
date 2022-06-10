573cc6921a28b9f370fac8a44639f32321fdc0a3
Michael <ih8e_mail@yahoo.com>
2022-06-10 00:54:23 -0400

    21:09

---
+++ src/components/TourCard.js
@@ -0,0 +1,10 @@
+import { Grid, Paper } from "@mui/material";
+
+const TourCard = () => {
+  return (
+    <Grid item xs={3}>
+      <Paper elevation={3}>Hello</Paper>
+    </Grid>
+  );
+};
+export default TourCard;
