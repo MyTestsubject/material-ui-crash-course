4974008286fba914f749bd5a10686b850b68d17f
Michael <ih8e_mail@yahoo.com>
2022-06-11 00:56:53 -0400

    51:37

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,9 +1,79 @@
-import { Grid, Paper } from "@mui/material";
+import { AccessTime } from "@mui/icons-material";
+import {
+  Box,
+  createTheme,
+  Grid,
+  Paper,
+  Rating,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+          },
+          props: {
+            variant: "body3",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
 
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>Hello</Paper>
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img
+            src="https://images.pexels.com/photos/1374585/pexels-photo-1374585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className="img"
+          />
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              Immerse into the Falls
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <AccessTime sx={{ width: 12.5 }} />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                5 Hours
+              </Typography>
+            </Box>
+            <Box marginTop={3} sx={{ display: "flex", alignItems: "center" }}>
+              <Rating size="small" precision={0.5} value={4.5} readOnly />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                4.5
+              </Typography>
+              <Typography variant="body3" component="p" marginLeft={1.5}>
+                (655 reviews)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h3" marginTop={0}>
+                From C $147
+              </Typography>
+            </Box>
+          </Box>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
