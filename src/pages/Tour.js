afc84cb8afba39080adba86a18c9551b5915645a
Michael <ih8e_mail@yahoo.com>
2022-06-13 17:51:11 -0400

    complete...ish

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,4 +1,13 @@
-import { Container, Typography } from "@mui/material";
+import {
+  BottomNavigation,
+  Box,
+  Container,
+  Paper,
+  Typography,
+} from "@mui/material";
+import CustomizedAccordions from "../components/Accordian";
+import ImageCollage from "../components/ImageCollage";
+import BasicModal from "../components/Modal";
 
 const Tour = () => {
   return (
@@ -6,6 +15,41 @@ const Tour = () => {
       <Typography variant="h3" component="h1" marginTop={3}>
         Explore the World in Vegas
       </Typography>
+      <Box marginTop={3} sx={{ display: "flex" }}>
+        <img
+          src="https://media.timeout.com/images/105124791/750/422/image.jpg"
+          height={325}
+        />
+        <ImageCollage />
+      </Box>
+      <Box>
+        <Typography variant="h6" component="h4" marginTop={3}>
+          About this ticket
+        </Typography>
+        <Typography variant="paragraph" component="p" marginTop={3}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit
+          voluptates rem quos delectus debitis earum modi, ipsum veritatis.
+          Perferendis officiis nobis, aut hic praesentium nulla vero, possimus
+          omnis reprehenderit blanditiis quis incidunt minima voluptatibus illum
+          quam corporis libero fugiat doloremque. Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Exercitationem officiis commodi beatae
+          animi incidunt necessitatibus aut provident ad ex. Saepe!
+        </Typography>
+      </Box>
+      <Box marginBottom={10}>
+        <Typography variant="h6" component="h4" marginBottom={2} marginTop={3}>
+          Frequently Asked Questions
+        </Typography>
+        <CustomizedAccordions />
+      </Box>
+      <Paper
+        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+        elevation={3}
+      >
+        <BottomNavigation>
+          <BasicModal />
+        </BottomNavigation>
+      </Paper>
     </Container>
   );
 };
